1ced55e10bea6ffe60e2f56b11237e1c79371057
Simon Mika <simon.mika@imint.se>
2013-06-07 13:30:44 +0200

    Fixed warnings.

--- Imint.Media.Test/Generator/Sine.cs
+++ Imint.Media.Test/Generator/Sine.cs
@@ -40,8 +40,6 @@ namespace Imint.Media.Test.Generator
         {
             this.Initialize();
         }
-        [Serialize.Parameter]
-        public Colorspace Format { get; set; }
         public override string Name
         {
             get { return "sine"; }
