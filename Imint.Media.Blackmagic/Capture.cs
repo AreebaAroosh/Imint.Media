b567ea66c36c234001d516427ee81d9175aa088f
David Hesselbom <david.hesselbom@imint.se>
2013-12-04 15:27:08 +0100

    New project: Imint.Media.Blackmagic. Perfomance issues remain.

---
+++ Imint.Media.Blackmagic/Capture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DeckLinkAPI;
+using System.Runtime.InteropServices;
+using Raster = Kean.Draw.Raster;
+using Buffer = Kean.Buffer;
+using Geometry2D = Kean.Math.Geometry2D;
+
+namespace Imint.Media.Blackmagic
+{
+	public class Capture :
+		IDeckLinkInputCallback
+	{
+		void IDeckLinkInputCallback.VideoInputFormatChanged(_BMDVideoInputFormatChangedEvents notificationEvents, IDeckLinkDisplayMode newDisplayMode, _BMDDetectedVideoInputFormatFlags detectedSignalFlags)
+		{
+			throw new NotImplementedException();
+		}
+
+		void IDeckLinkInputCallback.VideoInputFrameArrived(IDeckLinkVideoInputFrame videoFrame, IDeckLinkAudioInputPacket audioPacket)
+		{
+			IntPtr ip = new IntPtr();
+			videoFrame.GetBytes(out ip);
+			Raster.Image image = new Raster.Yuv420(new Buffer.Sized(ip, videoFrame.GetRowBytes() * videoFrame.GetHeight()),
+													new Geometry2D.Integer.Size(videoFrame.GetWidth(), videoFrame.GetHeight()));
+		}
+	}
+}
