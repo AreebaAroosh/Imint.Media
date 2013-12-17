c8200d545e20dc918a17dd46a1e777c12e50c619
David Hesselbom <david.hesselbom@imint.se>
2013-12-17 12:24:42 +0100

    Added blackmagic enums.

---
+++ Imint.Media.Blackmagic/Connection.cs
@@ -0,0 +1,18 @@
+﻿using DeckLinkAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Imint.Media.Blackmagic
+{
+	enum Connection
+	{
+		SDI = _BMDVideoConnection.bmdVideoConnectionSDI,
+		HDMI = _BMDVideoConnection.bmdVideoConnectionHDMI,
+		OpticalSDI = _BMDVideoConnection.bmdVideoConnectionOpticalSDI,
+		Component = _BMDVideoConnection.bmdVideoConnectionComponent,
+		Composite =_BMDVideoConnection.bmdVideoConnectionComposite,
+		SVideo = _BMDVideoConnection.bmdVideoConnectionSVideo,
+	}
+}
---
+++ Imint.Media.Blackmagic/Format.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeckLinkAPI;
+
+namespace Imint.Media.Blackmagic
+{
+	enum Format
+	{
+		frmt8BitARGB = _BMDPixelFormat.bmdFormat8BitARGB,
+		frmt8BitYUV = _BMDPixelFormat.bmdFormat8BitYUV,
+		frmt8BitBGRA = _BMDPixelFormat.bmdFormat8BitBGRA,
+		frmt10BitRGBX = _BMDPixelFormat.bmdFormat10BitRGBX,
+		frmt10BitRGBXLE = _BMDPixelFormat.bmdFormat10BitRGBXLE,
+		frmt10BitRGB = _BMDPixelFormat.bmdFormat10BitRGB,
+		frmt10BitYUV = _BMDPixelFormat.bmdFormat10BitYUV,
+	}
+}
