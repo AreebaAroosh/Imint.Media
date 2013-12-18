4941aede2b811508adc7afb12de7485e21c5938a
David Hesselbom <david.hesselbom@imint.se>
2013-12-18 14:21:26 +0100

    Blackmagic builds a nuget package.

---
+++ Imint.Media.Blackmagic/Install.ps1
@@ -0,0 +1,5 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$player = $project.ProjectItems.Item("Configuration").ProjectItems.Item("Player")
+ForEach ($item in $player.ProjectItems)
+		$item.Properties.Item("CopyToOutputDirectory").Value = 1
