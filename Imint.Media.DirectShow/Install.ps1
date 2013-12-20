8dd5904c5d3989e294b5029ea371f9e26d98ada1
David Hesselbom <david.hesselbom@imint.se>
2013-12-20 13:42:36 +0100

    Fixed nuspec and Install.ps1 in Imint.Media.DirectShow.

---
+++ Imint.Media.DirectShow/Install.ps1
@@ -0,0 +1,7 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$player = $project.ProjectItems.Item("Configuration").ProjectItems.Item("Player")
+ForEach ($item in $player.ProjectItems)
+{
+		$item.Properties.Item("CopyToOutputDirectory").Value = 1
+}
